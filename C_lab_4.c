3c723f83f2a205244e25a6443484a252f6a3e76e
krsna016 <016.krsna@gmail.com>
2022-10-08 09:57:08 +0530

    My first commit.

---
+++ C_lab_4.c
@@ -0,0 +1,18 @@
+/*
+Write a program in C to find the area of a circle as well as the circumference?
+(User can input a int or float.)
+*/
+
+
+# include <stdio.h>
+int main()
+{
+    float area,circumference,radius,pi = 3.14;
+    printf("Enter the value for the radius of the circle : ");
+    scanf("%f",&radius);
+    area = (pi*radius*radius);
+    circumference = (2*pi*radius);
+    printf("The area of the circle is : %f\n",area);
+    printf("The circumference of the circle is : %f\n",circumference);
+    return 0;
+}
---
+++ C_lab_5.c
@@ -0,0 +1,20 @@
+/*
+Write a program in C to find the area and perimeter 
+of a rectangle whose length and width are input by the user?
+(User can type int as well as float.)
+*/
+
+
+# include <stdio.h>
+int main()
+{
+    float area,perimeter,length,width;
+    printf("Enter the 'length' of the rectangle : ");
+    scanf("%f",&length);
+    printf("Enter the 'width' of the rectangle : ");
+    scanf("%f",&width);
+    area = (length*width);
+    perimeter = 2*(length + width);
+    printf("The area of the rectangle is : %f\n",area);
+    printf("The perimeter of the rectangle is : %f\n",perimeter);
+}
